ebc59c1d48aa4eda50036c8fee2f0cfddc58d32c
Garg, Anubhav <anubhav.d.garg@accenture.com>
2022-07-26 12:36:39 +0530

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,3 @@
-import { getLogger, Logger } from './utils/logging.service';
-import { ApiCallsService } from './utils/api-calls.service';
-import { ConfigService } from './utils/config.service';
-import { AzureService } from './utils/azure.config.service';
-
 import { BaseException } from './errorhelper/base-exception';
 import { CustomException } from './errorhelper/custom-exception';
 import { AuthGuard } from './guards/auth.guard';
@@ -10,6 +5,10 @@ import { UserValidationPipe } from './pipes/user-validation.pipe';
 import { ApicallInterceptor } from './utils/apicall.interceptor';
 import { ConstantsService } from './utils/constants.service';
 
+import { getLogger, Logger } from './utils/logging.service';
+import { ApiCallsService } from './utils/api-calls.service';
+import { ConfigService } from './utils/config.service';
+import { AzureService } from './utils/azure.config.service';
 export {
     getLogger, Logger, ApiCallsService, ConfigService, AzureService, BaseException, CustomException, AuthGuard,
     UserValidationPipe, ApicallInterceptor, ConstantsService
